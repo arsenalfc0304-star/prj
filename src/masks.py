3b27fdeee63961233eb390549457344ef0337547
дмитрий рыбин <arsenalfc0304@gmail.com>
2025-11-19 22:09:28 +0500

    перед добавлением функции mask_account_card

---
+++ src/masks.py
@@ -0,0 +1,18 @@
+def get_mask_card_number(card_number: str) -> str:
+    """
+    принимает на вход номер карты и возвращает ее маску
+    """
+    if not card_number.isdigit() or len(card_number) != 16:
+        return "Номер карты введен некорректно"
+    else:
+        return card_number[0:4] + " " + card_number[4:6] + "** **** " + card_number[12:]
+
+
+def get_mask_account(account_number: str) -> str:
+    """
+    принимает на вход номер счета и возвращает его маску
+    """
+    if not account_number.isdigit():
+        return "Номер счета введен некорректно"
+    else:
+        return "**" + account_number[-4:]
