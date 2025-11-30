7641d86ee5cccb1b6903e84df81e17aa637bf767
дмитрий рыбин <arsenalfc0304@gmail.com>
2025-11-30 19:10:05 +0500

    funcs developing

--- src/processing.py
+++ src/processing.py
@@ -1,6 +1,16 @@
-def filter_by_state(dict_list: list, key: str) -> list:
-    pass
+def filter_by_state(dict_list: list, state: str="EXECUTED") -> list:
+    """
+    принимает список словарей и опционально значение для ключа state (по умолчанию 'EXECUTED'),
+    возвращает новый список словарей, содержащий только словари, у которых ключ state соответствует указанному значению
+    """
+    return list(filter(lambda x: x["state"] == state, dict_list))
+
+
 def sort_by_date():
+    """
+    принимает список словарей и необязательный параметр, задающий порядок сортировки (по умолчанию — убывание),
+    возвращает новый список, отсортированный по дате (date)
+    """
     pass
 
 
