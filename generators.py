7f6d798d217ca1fb2f577e2d5f1f1404a2594bac
дмитрий рыбин <arsenalfc0304@gmail.com>
2025-12-20 18:17:26 +0500

    function and 1 generator of 2 ready

--- generators.py
+++ generators.py
@@ -4,9 +4,14 @@ def filter_by_currency(transactions, currency):
 
 
 transaction_descriptions = (x["description"] for x in transactions)
+transactions = [{"description": 1, "currency": "USB"}, {"description": 2, "currency": "USB"}]
+descriptions = transaction_descriptions(transactions)
+for _ in range(2):
+  print(next(descriptions))
 
 huy = filter_by_currency(transactions, "USB")
-print(next(huy))
-print(next(huy))
-print(next(transaction_descriptions))
-print(next(transaction_descriptions))
+for _ in range(2):
+    print(next(huy))
+
+#print(next(transaction_descriptions))
+#print(next(transaction_descriptions))
