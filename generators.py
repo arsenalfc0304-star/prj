1adb29d6bb13b606f6397da6e49bac7004f594cf
дмитрий рыбин <arsenalfc0304@gmail.com>
2025-12-20 17:32:34 +0500

    function and 1 generator of 2 ready

--- generators.py
+++ generators.py
@@ -1,3 +1,12 @@
-def filter_by_currency():
-    pass
+transactions = [{"description": 1, "currency": "USB"}, {"description": 2, "currency": "USB"}]
+def filter_by_currency(transactions, currency):
+    return filter(lambda item: item["currency"] == currency, transactions)
 
+
+transaction_descriptions = (x["description"] for x in transactions)
+
+huy = filter_by_currency(transactions, "USB")
+print(next(huy))
+print(next(huy))
+print(next(transaction_descriptions))
+print(next(transaction_descriptions))
