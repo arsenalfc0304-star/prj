16488213625d1fe630d5df83e467f22756e53392
дмитрий рыбин <arsenalfc0304@gmail.com>
2025-12-21 16:09:12 +0500

    generators.py ready

--- generators.py
+++ generators.py
@@ -1,17 +1,127 @@
-transactions = [{"description": 1, "currency": "USB"}, {"description": 2, "currency": "USB"}]
+transactions = (
+    [
+        {
+            "id": 939719570,
+            "state": "EXECUTED",
+            "date": "2018-06-30T02:08:58.425572",
+            "operationAmount": {
+                "amount": "9824.07",
+                "currency": {
+                    "name": "USD",
+                    "code": "USD"
+                }
+            },
+            "description": "Перевод организации",
+            "from": "Счет 75106830613657916952",
+            "to": "Счет 11776614605963066702"
+        },
+        {
+            "id": 142264268,
+            "state": "EXECUTED",
+            "date": "2019-04-04T23:20:05.206878",
+            "operationAmount": {
+                "amount": "79114.93",
+                "currency": {
+                    "name": "USD",
+                    "code": "USD"
+                }
+            },
+            "description": "Перевод со счета на счет",
+            "from": "Счет 19708645243227258542",
+            "to": "Счет 75651667383060284188"
+        },
+        {
+            "id": 873106923,
+            "state": "EXECUTED",
+            "date": "2019-03-23T01:09:46.296404",
+            "operationAmount": {
+                "amount": "43318.34",
+                "currency": {
+                    "name": "руб.",
+                    "code": "RUB"
+                }
+            },
+            "description": "Перевод со счета на счет",
+            "from": "Счет 44812258784861134719",
+            "to": "Счет 74489636417521191160"
+        },
+        {
+            "id": 895315941,
+            "state": "EXECUTED",
+            "date": "2018-08-19T04:27:37.904916",
+            "operationAmount": {
+                "amount": "56883.54",
+                "currency": {
+                    "name": "USD",
+                    "code": "USD"
+                }
+            },
+            "description": "Перевод с карты на карту",
+            "from": "Visa Classic 6831982476737658",
+            "to": "Visa Platinum 8990922113665229"
+        },
+        {
+            "id": 594226727,
+            "state": "CANCELED",
+            "date": "2018-09-12T21:27:25.241689",
+            "operationAmount": {
+                "amount": "67314.70",
+                "currency": {
+                    "name": "руб.",
+                    "code": "RUB"
+                }
+            },
+            "description": "Перевод организации",
+            "from": "Visa Platinum 1246377376343588",
+            "to": "Счет 14211924144426031657"
+        }
+    ]
+)
 def filter_by_currency(transactions, currency):
+    """
+    принимает на вход список словарей, представляющих транзакции,
+    возвращает итератор, который поочередно выдает транзакции, где валюта операции соответствует заданной
+    """
     return filter(lambda item: item["currency"] == currency, transactions)
 
 
-transaction_descriptions = (x["description"] for x in transactions)
-transactions = [{"description": 1, "currency": "USB"}, {"description": 2, "currency": "USB"}]
+def transaction_descriptions(transactions):
+    """
+    принимает список словарей с транзакциями,
+    возвращает описание каждой операции по очереди
+    """
+
+    for x in transactions:
+        yield x["description"]
+
+
+def card_number_generator(start, stop):
+    """
+    принимает начальное и конечное значения для генерации диапазона номеров карт.
+    выдает номера банковских карт в формате XXXX XXXX XXXX XXXX, где X — цифра номера карты.
+    Генератор может сгенерировать номера карт в заданном диапазоне от 0000 0000 0000 0001 до 9999 9999 9999 9999.
+    """
+    x = start
+    while 1 <= x <= stop <= 9999999999999999:
+        str1 = ""
+        for y in range(0, 16-len(str(x))):
+            str1 += "0"
+        str1 += str(x)
+        yield str1
+        x += 1
+
 descriptions = transaction_descriptions(transactions)
-for _ in range(2):
+
+# huy = filter_by_currency(transactions, "USB")
+# for _ in range(2):
+#     print(next(huy))
+
+for card_number in card_number_generator(9999999999999987, 9999999999999999):
+    print(card_number)
+#print(next(card_number))
+#print(next(card_number))
+
+
+for _ in range(3):
   print(next(descriptions))
 
-huy = filter_by_currency(transactions, "USB")
-for _ in range(2):
-    print(next(huy))
-
-#print(next(transaction_descriptions))
-#print(next(transaction_descriptions))
