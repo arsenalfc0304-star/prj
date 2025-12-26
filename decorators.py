465628e593b1c4f987a062e446960bc8ddeffe39
дмитрий рыбин <arsenalfc0304@gmail.com>
2025-12-26 20:21:48 +0500

    decs, test_decs in progress

--- decorators.py
+++ decorators.py
@@ -6,14 +6,19 @@ def log(filename):
     def decorator(func):
         def wrapper(*args, **kwargs):
             time_1 = time()
-            result = func(*args, **kwargs)
+            try:
+                if not filename == "":
+                    with open(filename, "r") as f:
+                        with open(filename, "a") as file:
+                            file.write(result + "\n")
+                else:
+                    print(result)
+                result = func(*args, **kwargs)
+                return result
+            except Exception as e:
+                print(f"Retrying... ({e})")
+            raise Exception(f"Something went wrong!")
             time_2 = time()
-            if not filename == "":
-                with open(filename, "r") as f:
-                    with open(filename, "a") as file:
-                        file.write(result + "\n")
-            else:
-                print(result)
 
         return wrapper
 
--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -1,17 +1,12 @@
 import pytest
+from decorators import log
 
 
-def test_log(capsys):
-    captured = capsys.readouterr()
-    assert captured.out == "Hello, world!\n"
-
-
-@retry_decorator(max_retries=3)
+@log("")
 def example_function():
-    # some potentially failing operation
-    raise ValueError("Something went wrong!")
+    raise Exception("Something went wrong!")
 
 
-def test_retry_decorator():
-    with pytest.raises(Exception, match="Max retries exceeded"):
+def test_exception_log():
+    with pytest.raises(Exception, match="Something went wrong!"):
         example_function()
