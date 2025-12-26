147957f165a1eafac363fda6ec599e43131b9c09
дмитрий рыбин <arsenalfc0304@gmail.com>
2025-12-27 00:26:23 +0500

    decs, test_decs in progress

--- decorators.py
+++ decorators.py
@@ -2,24 +2,30 @@ import os
 from time import time
 
 
-def log(filename):
+def log():
     def decorator(func):
         def wrapper(*args, **kwargs):
-            time_1 = time()
+           # time_1 = time()
             try:
-                if not filename == "":
-                    with open(filename, "r") as f:
-                        with open(filename, "a") as file:
-                            file.write(result + "\n")
-                else:
-                    print(result)
                 result = func(*args, **kwargs)
-                return result
+                # if not filename == "":
+                #     with open(filename, "r") as f:
+                #         with open(filename, "a") as file:
+                #             file.write(result + "\n")
+                # else:
             except Exception as e:
-                print(f"Retrying... ({e})")
-            raise Exception(f"Something went wrong!")
-            time_2 = time()
+                raise Exception(f"Max retries exceeded")
+           # time_2 = time()
+            return result
+            print(time_1)
+            print(time_2)
 
         return wrapper
 
     return decorator
+#
+# @log()
+# def example_func(a, b):
+#     return a / b
+#
+# example_func(5, 2)
--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -8,6 +8,11 @@ def test_log():
         return a / b
 
     result = func(4, 2)
-    assert result == 2
+    assert result == 2.0
     # with pytest.raises(Exception, match="Something went wrong!"):
     #     example_function()
+
+def test_exception_log():
+    @log
+    result = func(4, 0)
+    with pytest.raises(Exception, match="Something went wrong!")
