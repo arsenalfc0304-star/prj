e34975fc82559b872d06bd65e43600f10000b702
дмитрий рыбин <arsenalfc0304@gmail.com>
2025-12-26 19:45:22 +0500

    decs, test_decs in progress

--- decorators.py
+++ decorators.py
@@ -1,17 +1,17 @@
 import os
 
+
 def log(filename):
     def decorator(func):
         def wrapper(*args, **kwargs):
             result = func(*args, **kwargs)
             if os.path.exists(filename):
                 with open(filename, "r") as f:
-                    with open(f'{filename}.txt', 'a') as file:
-                        file.write(result + '\n')
+                    with open(f"{filename}.txt", "a") as file:
+                        file.write(result + "\n")
             else:
                 print(result)
 
         return wrapper
+
     return decorator
-
-
