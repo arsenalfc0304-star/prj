dbcd734f984e3faec888b8ef26cd8eda9c862d5d
дмитрий рыбин <arsenalfc0304@gmail.com>
2026-01-02 16:02:46 +0500

    hw 12_1 - utils, test_utils in progress

--- src/utils.py
+++ src/utils.py
@@ -1,9 +1,25 @@
 import json
 
+
 def load_json(path):
-    with open(path, encoding='utf-8') as f:
-        data = json.load(f)
-    return data
+    """
+    принимает на вход путь до JSON-файла и возвращает список словарей с данными о финансовых транзакциях.
+    Если файл пустой, содержит не список или не найден, функция возвращает пустой список.
+    """
+    try:
+        with open(path, encoding="utf-8") as f:
+            data = json.load(f)
+            return data
+    except Exception:
+        return []
 
 
-print(load_json('data/operations.json'))
+print(load_json("data/operations.json"))
+
+
+def transaction_sum():
+    """
+    принимает на вход транзакцию и возвращает сумму транзакции (amount) в рублях, тип данных — float.
+    Если транзакция была в USD или EUR,
+    происходит обращение к внешнему API для получения текущего курса валют и конвертации суммы операции в рубли
+    """
