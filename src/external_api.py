3e71ef52675eb5ffb91304c5eba5fdd96e2c0bc0
дмитрий рыбин <arsenalfc0304@gmail.com>
2026-01-03 01:51:28 +0500

    hw 12_1 - utils, test_utils in progress

--- src/external_api.py
+++ src/external_api.py
@@ -1,6 +1,8 @@
 import requests
-
-
+import os
+from dotenv import load_dotenv
+load_dotenv()
+apikey = os.getenv("API_KEY")
 def convert_to_rub(amount: float, currency_from: str) -> float:
     url = "https://api.apilayer.com/exchangerates_data/convert"
     payload = {
@@ -9,13 +11,13 @@ def convert_to_rub(amount: float, currency_from: str) -> float:
         "to": "RUB"
         }
     headers = {
-        "apikey": "Sa5AQJe8Ko8JXKWBVMuTRS9OhiRrNoOK"
+        "apikey": apikey
         }
 
     response = requests.get(url, headers=headers, params=payload)
     status_code = response.status_code
     result = response.json()
     if status_code == 200:
-        return result["result"]
+        print(result["result"])
 
 #convert_to_rub(amount=100, currency_from="USD")
