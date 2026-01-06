7d36a8517852b841c3c78290c3a2a50df87fc7a6
дмитрий рыбин <arsenalfc0304@gmail.com>
2026-01-06 20:23:01 +0500

    hw 12_2 loggers.py in progress

---
+++ loggers.py
@@ -0,0 +1,8 @@
+import logging
+
+
+utils_logger = logging.getLogger("utils")
+utils_file_handler = logging.FileHandler("./logs/utils.log")
+utils_file_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+utils_file_handler.setFormatter(utils_file_formatter)
+utils_logger.addHandler(utils_file_handler)
