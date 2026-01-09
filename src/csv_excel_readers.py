6942369bedbd6ec211c9e6c5af71c6fab426528b
дмитрий рыбин <arsenalfc0304@gmail.com>
2026-01-09 16:11:29 +0500

    hw 13_1 in progress

--- src/csv_excel_readers.py
+++ src/csv_excel_readers.py
@@ -2,8 +2,23 @@ import pandas as pd
 
 
 def read_data_from_csv(path: str) -> list:
+    """
+    Функция для считывания финансовых операций из CSV принимает путь к файлу CSV в качестве аргумента,
+    выдает список словарей с транзакциями
+    """
     data = pd.read_csv(path).head()
     print(data.to_dict(orient='list'))
 
 
-read_data_from_csv("data/transactions.csv")
+def read_data_from_excel(path: str) -> list:
+    """
+    Функция для считывания финансовых операций из Excel принимает путь к файлу Excel в качестве аргумента,
+    выдает список словарей с транзакциями
+    """
+    data = pd.read_excel(path)
+    print(data.to_dict(orient='records'))
+
+
+#read_data_from_csv("data/transactions.csv")
+
+read_data_from_excel("data/transactions_excel.xlsx")
