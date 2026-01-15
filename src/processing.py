9300e678e88cffa624f3e893bc160348b372a988
дмитрий рыбин <arsenalfc0304@gmail.com>
2026-01-15 22:56:53 +0500

    hw 13_2 in progress

--- src/processing.py
+++ src/processing.py
@@ -1,6 +1,7 @@
 import re
 from collections import Counter
 
+
 def filter_by_state(dict_list: list, state: str = "EXECUTED") -> list:
     """
     принимает список словарей и опционально значение для ключа state (по умолчанию "EXECUTED"),
@@ -17,19 +18,19 @@ def sort_by_date(dict_list: list, descending: bool = True) -> list:
     return sorted(dict_list, key=lambda x: x["date"], reverse=descending)
 
 
-def process_bank_search(dict_list:list[dict], search:str)->list[dict]:
+def process_bank_search(dict_list: list[dict], search: str) -> list[dict]:
     """
     принимает список словарей с данными о банковских операциях и строку поиска,
     возвращает список словарей, у которых в описании есть данная строка
     """
     filtered_dict_list = []
     for transaction in dict_list:
-        if not re.search(search, transaction["description"]) == None:
+        if not re.search(search, transaction["description"]) is None:
             filtered_dict_list.append(transaction)
     return filtered_dict_list
 
 
-def process_bank_operations(dict_list:list[dict], categories:list)->dict:
+def process_bank_operations(dict_list: list[dict], categories: list) -> dict:
     """
     принимает список словарей с данными о банковских операциях и список категорий операций,
     возвращает словарь (ключи — это названия категорий, значения — это количество операций в каждой категории)
