0879d6fd05f1077c47633820ab472e165d902f48
дмитрий рыбин <arsenalfc0304@gmail.com>
2026-01-27 23:10:27 +0500

    hw 13_2 ready

--- main.py
+++ main.py
@@ -112,7 +112,7 @@ def main():
             print(
                 f"{transaction['date'][8:10]}.{transaction['date'][5:7]}.{transaction['date'][0:4]} {transaction['description']}\n"
                 f"{mask_account_card(transaction['to'])}\n"
-                f"Сумма: {transaction['amount']} {transaction['currency']['name']}"
+                f"Сумма: {transaction['amount']} {transaction['currency_name']}"
             )
     else:
         print("Программа: Не найдено ни одной транзакции, подходящей под ваши условия фильтрации")
